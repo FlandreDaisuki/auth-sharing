8a1e8ea771760f9d1165aa4c3b51a65bf0647ff2
FlandreDaisuki <vbnm123c@gmail.com>
2022-03-05 16:59:22 +0800

    Add cookie-session

---
+++ cookie-session/app.mjs
@@ -0,0 +1,68 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import cookieParser from 'cookie-parser';
+
+import fs from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const userHTMLTemplate = fs.readFileSync('views/user.html', 'utf8');
+
+const getGithubUser = async(githubUsername) => {
+  const response = await fetch(`https://api.github.com/users/${githubUsername}`);
+  return await response.json();
+};
+
+const applyUserHTML = async(user) => {
+  const githubUser = await getGithubUser(user.github);
+  return userHTMLTemplate
+    .replace(/{{\s*name\s*}}/g, user.name)
+    .replace(/{{\s*avatar.src\s*}}/g, githubUser.avatar_url)
+    .replace(/{{\s*avatar.alt\s*}}/g, `${user.name}'s avatar`)
+    .replace(/{{\s*github\s*}}/g, user.github)
+    .replace(/{{\s*id\s*}}/g, user.id); // You can replace all id for keep session
+};
+
+const connectDatabase = async() => {
+  const response = await fetch('http://database/db');
+  return await response.json();
+};
+
+const app = express();
+
+app.use(cookieParser());
+app.get('/', async(req, res) => {
+  const db = await connectDatabase();
+  console.log('GET', 'req.cookies', req.cookies);
+
+  const foundUser = db.find((record) => String(record.id) === req.cookies.id);
+  console.log('GET', req.url, foundUser);
+
+  if (foundUser) {
+    res.send(await applyUserHTML(foundUser));
+  } else {
+    res.sendFile('views/login.html', { root: __dirname });
+  }
+});
+
+app.get('/logout', async(req, res) => {
+  res.cookie('id', '', { expires: new Date(0) }).redirect('/');
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.post('/', async(req, res) => {
+  console.log('POST', req.url, req.body);
+  const db = await connectDatabase();
+  const foundUser = db.find(({ name, password }) =>
+    name === req.body.name && password === req.body.password,
+  );
+  console.log('foundUser', foundUser);
+  if (foundUser) {
+    res.cookie('id', foundUser.id, { httpOnly: true }).redirect('/');
+  } else {
+    res.redirect('/');
+  }
+});
+
+app.listen(80);
