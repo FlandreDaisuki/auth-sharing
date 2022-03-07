2471423a46b25637ed1419a5ff0a5d3a4060979c
FlandreDaisuki <vbnm123c@gmail.com>
2022-03-08 00:16:34 +0800

    Add entry index

--- entry/app.mjs
+++ entry/app.mjs
@@ -1,9 +1,10 @@
 import express from 'express';
+import fs from 'fs/promises';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello');
+app.get('/', async(req, res) => {
+  res.send(await fs.readFile('views/index.html', 'utf8'));
 });
 
 app.listen(80);
